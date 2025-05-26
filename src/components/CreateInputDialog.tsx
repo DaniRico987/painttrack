dd5dedd5ba7e61fe961aa487dbe1c13aa4ee5867
daniel.rico <daniel.rico@utp.edu.co>
2025-05-26 12:51:15 -0500

    fix: Corrected onChange handler for unitPrice field in CreateInputDialog

--- src/components/CreateInputDialog.tsx
+++ src/components/CreateInputDialog.tsx
@@ -75,7 +75,7 @@ const CreateInputDialog = ({ open, onClose, onCreate }: Props) => {
             label="Precio por unidad"
             type="number"
             value={form.unitPrice}
-            onChange={(e) => handleChange("type", e.target.value)}
+            onChange={(e) => handleChange("unitPrice", e.target.value)}
             fullWidth
           />
           <TextField
